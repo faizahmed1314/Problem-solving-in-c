1b2f33f90ad774f2aa2782bd715ff70066be6fb1
faiz personel <foyezahmedfj20@gmail.com>
2025-02-27 15:50:05 +0600

    print capital latter

--- ComputerProgrammingPart2/ComputerProgrammingPart2/ComputerProgrammingPart2.cpp
+++ ComputerProgrammingPart2/ComputerProgrammingPart2/ComputerProgrammingPart2.cpp
@@ -1,20 +1,18 @@
-// ComputerProgrammingPart2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<stdio.h>
 
-#include <iostream>
+int main() {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	// Print Ascii character
+	//for (int i = 33; i <= 126; i++) {
+	//	printf("ASCII code for %c is %d\n", i, i);
+	//}
+
+	// Print capital latter
+	char small_latter;
+	char capital_latter;
+	printf("Enter a small letter : \n");
+	small_latter = getchar();
+	capital_latter = small_latter - 32;
+	printf("The capital letter is %c\n", capital_latter);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
