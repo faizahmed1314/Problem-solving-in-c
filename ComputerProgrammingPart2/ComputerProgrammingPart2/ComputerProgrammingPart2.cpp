6b692d1baff20d3e2743ad532ac66e82cac6f015
faiz personel <foyezahmedfj20@gmail.com>
2025-02-27 16:09:26 +0600

    check the size of variable

--- ComputerProgrammingPart2/ComputerProgrammingPart2/ComputerProgrammingPart2.cpp
+++ ComputerProgrammingPart2/ComputerProgrammingPart2/ComputerProgrammingPart2.cpp
@@ -8,11 +8,54 @@ int main() {
 	//}
 
 	// Print capital latter
-	char small_latter;
-	char capital_latter;
-	printf("Enter a small letter : \n");
-	small_latter = getchar();
-	capital_latter = small_latter - 32;
-	printf("The capital letter is %c\n", capital_latter);
+	//char small_latter;
+	//char capital_latter;
+	//printf("Enter a small letter : \n");
+	//small_latter = getchar();
+	//capital_latter = small_latter - 32;
+	//printf("The capital letter is %c\n", capital_latter);
+
+	//print memory address
+	//int a = 10;
+	//int b = 20;
+	//char c = 'A';
+	//char d = 'B';
+
+	//printf("The value of a is %d\n", a);
+	//printf("The memory address of a is %p\n", &a);
+
+	//printf("The value of b is %d\n", b);
+	//printf("The memory address of b is %p\n", &b);
+
+	//printf("The value of c is %c\n", c);
+	//printf("The memory address of c is %p\n", &c);
+
+	//printf("The value of d is %c\n", d);
+	//printf("The memory address of d is %p\n", &d);
+
+	//print memory address of an array
+
+	//int arr[5] = { 10, 20, 30, 40, 50 };
+
+	//printf("The memory address of arr is %p\n", arr);
+
+	//for (int i = 0; i < 5; i++) {
+	//	printf("The value of arr[%d] is %d\n", i, arr[i]);
+	//	printf("The memory address of arr[%d] is %p\n", i, &arr[i]);
+	//}
+
+	// find the size of different type of variable
+
+	int num;
+	float ammount;
+	char name;
+	bool isTrue;
+
+	printf("%lu\n", sizeof(int));
+
+	printf("The size of int %d\n", sizeof(num));
+	printf("The size of float %d\n", sizeof(ammount));
+	printf("The size of char %d\n", sizeof(name));
+	printf("The size of bool %d\n", sizeof(isTrue));
 	return 0;
 }
