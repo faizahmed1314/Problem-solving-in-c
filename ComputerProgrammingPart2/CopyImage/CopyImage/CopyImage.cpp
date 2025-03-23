c9485fd57b653547d3d70ab39fa2af9c880846e5
faiz personel <foyezahmedfj20@gmail.com>
2025-03-24 00:37:19 +0600

    find the file size

--- ComputerProgrammingPart2/CopyImage/CopyImage/CopyImage.cpp
+++ ComputerProgrammingPart2/CopyImage/CopyImage/CopyImage.cpp
@@ -18,6 +18,8 @@ int main() {
 		return -1;
 	}
 
+
+
 	if (err2 != 0) {
 		perror("File opening failed");
 		fclose(fp1);
@@ -27,6 +29,11 @@ int main() {
 	while ((bytesRead = fread(buffer, 1, sizeof(buffer), fp1)) > 0) {
 		fwrite(buffer, 1, bytesRead, fp2);
 	}
+
+	fseek(fp1, 0, SEEK_END);
+
+	printf("File size : %ld bytes\n", ftell(fp1));
+	printf("File size : %ld kilobytes\n", ftell(fp1) / 1024);
 
 	fclose(fp1);
 	fclose(fp2); 
